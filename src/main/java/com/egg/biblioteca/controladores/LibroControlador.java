cc2636a70584666b150689a09084d4fd76520582
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2025-03-05 16:45:13 -0300

    Modifica LibroControlador

--- src/main/java/com/egg/biblioteca/controladores/LibroControlador.java
+++ src/main/java/com/egg/biblioteca/controladores/LibroControlador.java
@@ -1,12 +1,17 @@
 package com.egg.biblioteca.controladores;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.egg.biblioteca.entidades.Autor;
+import com.egg.biblioteca.entidades.Editorial;
 import com.egg.biblioteca.excepciones.MiException;
 import com.egg.biblioteca.servicios.AutorServicio;
 import com.egg.biblioteca.servicios.EditorialServicio;
@@ -23,22 +28,28 @@ public class LibroControlador {
     private EditorialServicio editorialServicio;
 
     @GetMapping("/registrar")
-    public String registrar(){
+    public String registrar(ModelMap modelo){
+        List<Autor> autores = autorServicio.listarAutores();
+        List<Editorial> editoriales = editorialServicio.listarEditoriales();
+        modelo.addAttribute("autores", autores);
+        modelo.addAttribute("editoriales", editoriales);
         return "libro_form.html";
     }
 
-     @PostMapping("/registro")
-  public String registro(@RequestParam(required = false) Long isbn, @RequestParam String titulo, 
- 	     @RequestParam(required = false) Integer ejemplares, @RequestParam Long idAutor,
-            @RequestParam Long idEditorial) {
+    @PostMapping("/registro")
+    public String registro(@RequestParam(required = false) Long isbn, 
+                           @RequestParam String titulo, 
+                           @RequestParam(required = false) Integer ejemplares, 
+                           @RequestParam String idAutor,
+                           @RequestParam String idEditorial,
+                           ModelMap modelo) {
         try {
             libroServicio.crearLibro(isbn, titulo, ejemplares, idAutor, idEditorial);
-
-
+            modelo.put("exito", "Libro registrado correctamente.");
         } catch (MiException ex) {
-            
-            return "libro_form.html"; // volvemos a cargar el formulario.
+            modelo.put("error", ex.getMessage());
+            return "libro_form.html"; // volvemos a cargar el formulario con el error.
         }
         return "index.html";
-    }
+    }    
 }
